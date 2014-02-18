e65f779c138d0396923442dd7287b74f196145fc
nh0815 <nh0815@gmail.com>
2014-02-18 10:31:30 -0500

    Implemented basic stopword remover.  Reads stopwords from file and filters out those words from input.

--- src/stopword.py
+++ src/stopword.py
@@ -1,1 +1,15 @@
 __author__ = 'Nick Hirakawa'
+
+
+class StopwordRemover:
+
+    def __init__(self, stop_file):
+        with open(stop_file, 'r') as f:
+            self.stopwords = f.readlines()
+        self.output = []
+
+
+    def feed(self, input):
+        buffer = [x for x in input if x in self.stopwords]
+        for word in buffer:
+            self.output.append(word)
